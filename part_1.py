2086d33cd0ef6a88d492b2aa548f01491d4b0e73
dm17-246 <dm17@kent.ac.uk>
2026-02-10 12:56:20 +0000

    Part 1 complete

--- part_1.py
+++ part_1.py
@@ -1,1 +1,19 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+print (sample_bay[0])
+print(sample_bay[-1])
+print(len(sample_bay))
+
+x=3
+sample_bay.remove(sample_bay[x])
+
+for sample in sample_bay:
+    print("Transmitting data for: " + sample)
+
+new_findings = []
+
+for findings in range(3):
+    new_finding = input("Enter new finding: ")
+    new_findings.append(new_finding.capitalize())
+
+print("New findings: " + str(new_findings))
 
